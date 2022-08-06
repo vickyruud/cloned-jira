48e01cc87723d831c7bb50e27453cc534fc4b684
vickyruud <aprajit.k@gmail.com>
2022-08-06 10:29:03 -0400

    set up task and user types

---
+++ graphql/types/User.ts
@@ -0,0 +1,25 @@
+import { objectType, extendType, stringArg, nonNull, intArg } from "nexus";
+import { Task } from "./Task";
+
+export const User = objectType({
+  name: "User",
+  definition(t) {
+    t.string("id");
+    t.string("name");
+    t.string("email");
+    t.string("image");
+    t.field("role", { type: Role });
+    t.list.field("tasks", {
+      type: Task,
+      async resolve(parent, _args, ctx) {
+        return await ctx.prisma.user
+          .findUnique({
+            where: {
+              id: parent.id,
+            },
+          })
+          .tasks();
+      },
+    });
+  },
+});
